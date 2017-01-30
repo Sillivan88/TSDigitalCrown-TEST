94357b1eed391689ee3e1d38fad788737ff1559f
Sillivan88 <thomas@thomassillmann.de>
2017-01-30 16:21:09 +0100

    Initial Commit

---
+++ Digitaion Crown [TEST] WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Digitaion Crown [TEST] WatchKit Extension
+//
+//  Created by Thomas Sillmann on 30.01.17.
+//  Copyright © 2017 Thomas Sillmann. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
